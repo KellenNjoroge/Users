b16bb82516fc3557b894fb98f56d122c29eb8884
KellenNjoroge <muthonkel@gmail.com>
2018-08-17 15:36:47 +0300

    user-form change

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-
+import {Users} from '../users';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
+    get Users() {
+      return Users;
+    }
   constructor() { }
 }
