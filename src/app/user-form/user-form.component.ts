0eae6c91163dcf31a69d50b423a53f8130836570
KellenNjoroge <muthonkel@gmail.com>
2018-08-17 14:27:26 +0300

    ngModel works

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {User} from '../user';
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
-
+  newUser = new User (0, '', '');
   constructor() { }
 
   ngOnInit() {
