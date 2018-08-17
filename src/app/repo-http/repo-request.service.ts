ef167fa50307c371b109cbe2d40ced9f3ca914f2
KellenNjoroge <muthonkel@gmail.com>
2018-08-17 21:44:01 +0300

    promise API

--- src/app/repo-http/repo-request.service.ts
+++ src/app/repo-http/repo-request.service.ts
@@ -1,11 +1,38 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
-import {Repo} from '../../repo-class/repo';
+import {Repo} from '../repo-class/repo';
 @Injectable({
   providedIn: 'root'
 })
 export class RepoRequestService {
+  repo: Repo;
+  constructor(private http: HttpClient) {
+    this.repo = new Repo('', '');
+  }
+  repoRequest() {
 
-  constructor() { }
+    interface ApiResponse {
+        name: any;
+        location: any;
+
+    }
+    const promise = new Promise((resolve, reject) => {
+        this.http.get<ApiResponse>(environment.apiUrl).toPromise().then(response => {
+            this.repo.name = response.name;
+            this.repo.location = response.location;
+
+            resolve();
+        },
+        error => {
+                this.repo.name = 'Kellen_Njoroge.';
+                this.repo.location = 'Nairobi_Kenya';
+                reject(error);
+            }
+        );
+    });
+
+    return promise;
+  }
 }
+
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,22 +4,20 @@ import {UserService} from '../users/user.service';
 import { User } from '../user';
 import {HttpClient} from '@angular/common/http';
 import {Repo} from '../repo-class/repo';
+import {RepoRequestService} from '../repo-http/repo-request.service';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  providers: [UserService],
+  providers: [UserService, RepoRequestService],
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
   user = Users;
   repo: Repo;
   users: User[];
-  constructor(userService: UserService, private http: HttpClient) {
+  constructor(userService: UserService, private http: HttpClient, private repoService: RepoRequestService)  {
   this.users = userService.getUsers();
    }
-  // users = [
-  //   new User(1, 'Kellen', '30 repositories so far'),
-  // ];
   toogleDetails(index) {
     this.users[index].showDescription = !this.users[index].showDescription;
 }
@@ -32,18 +30,8 @@ addNewUser(user) {
   // tslint:disable-next-line:member-ordering
 
   ngOnInit() {
-    interface ApiResponse {
-        name: any;
-        location: any;
-    }
-    // tslint:disable-next-line:max-line-length
-    this.http.get<ApiResponse>('https://api.github.com/users/KellenNjoroge?access_token=ed1ef4560933cae249b21bf58136daf47b546069').subscribe(data => {
-      this.repo = new Repo(data.name, data.location);
-        // Successful API request.
-    }, err => {
-      this.repo = new Repo('Kellen_Njoroge', 'Nairobi_Kenya');
-      console.log('Error occured ');
-  });
+    this.repoService.repoRequest();
+    this.repo = this.repoService.repo;
   }
 
 }
