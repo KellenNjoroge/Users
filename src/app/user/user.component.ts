e6d181d47c43293eeacfe879c66d12d0fa7cf435
KellenNjoroge <muthonkel@gmail.com>
2018-08-17 15:15:39 +0300

    user service

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from '../user';
+import {Users} from '../users';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  users = [
-    new User(1, 'Kellen', '30 repositories so far'),
-  ];
+  users = Users;
+  // users = [
+  //   new User(1, 'Kellen', '30 repositories so far'),
+  // ];
   toogleDetails(index) {
     this.users[index].showDescription = !this.users[index].showDescription;
 }
---
+++ src/app/users.ts
@@ -0,0 +1,4 @@
+import {User} from './user';
+export const Users: User[] = [
+  new User(1, 'Kellen', '30 repositories so far')
+];
