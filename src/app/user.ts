8ab60c8937233c39a11e0229e01c3df086a1d490
KellenNjoroge <muthonkel@gmail.com>
2018-08-20 10:34:09 +0300

    api url

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,6 +1,6 @@
 export class User {
   public showDescription: boolean;
-  constructor(public id: number, public name: string, public repos_url: string) {
+  constructor(public id: number, public name: string, public repos_url: any) {
     this.showDescription = false;
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://api.github.com/users/KellenNjoroge?access_token=ed1ef4560933cae249b21bf58136daf47b546069'
+  apiUrl: 'https://api.github.com/users/KellenNjoroge?access_token=ed1ef4560933cae249b21bf58136daf47b546069',
+  repoUrl: 'https://api.github.com/users/KellenNjoroge/repos?access_token=ed1ef4560933cae249b21bf58136daf47b546069',
 };
 
 /*
