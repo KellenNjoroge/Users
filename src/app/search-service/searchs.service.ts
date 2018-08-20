55285506f35cc5a3e8ca718e0eb5384107c7c3a4
KellenNjoroge <muthonkel@gmail.com>
2018-08-20 12:38:04 +0300

    commented out search service2

--- src/app/search-service/searchs.service.ts
+++ src/app/search-service/searchs.service.ts
@@ -1,27 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
-import { toPromise } from 'rxjs/operators/toPromise';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchsService {
   // tslint:disable-next-line:no-inferrable-types
-  apiRoot: string = 'https://api.github.com';
-  results: Object[];
-  constructor(private http: Http) {
-    this.results = [];
-  }
+//   apiRoot: string = 'https://api.github.com';
+//   results: Object[];
+//   constructor(private http: Http) {
+//     this.results = [];
+//   }
 //  search(term: string) {
 //  const promise = new Promise((resolve, reject) => {
 //   const apiURL =  `${this.apiRoot}?term=$user`;
 //   this.http.get(apiURL);
-//    .toPromise()
-//    .then(
-//      res => {
-//       console.log(res.json());
-//      });
-//      return promise;
-//  })
+// //    .toPromise()
+// //    .then(
+// //      res => {
+// //       console.log(res.json());
+// //      });
+// //      return promise;
+// //  })
 //  }
 }
